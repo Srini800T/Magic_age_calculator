9b4afd7b4409ad7420990087b72ca8cc4dff0354
Srini800T <sri5ragaventra@gmail.com>
2025-10-30 18:52:51 +0530

    Add files via upload

---
+++ magic age calculator.py
@@ -0,0 +1,14 @@
+age=int(input("Enter your mobile number last two digits:"))
+result = age * 2
+result = result + 5
+result = result * 50
+result = result + 1775
+
+birth_year = int(input("Enter your birth year:"))
+result = result - birth_year
+print("Your final magic number is: ", result)
+print("Your phone number(last two digits) appears in the first part",result)
+print("Your age appears in the last two digits ",result)
+
+
+
